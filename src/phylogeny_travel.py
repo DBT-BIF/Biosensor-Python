4c66313c16cd89c812109eccda066f771bd2412d
bharatpurohit97 <006.rajpurohit@gmail.com>
2018-09-27 18:35:22 +0530

    update and edit

--- src/phylogeny_travel.py
+++ src/phylogeny_travel.py
@@ -79,34 +79,32 @@ class TravelPhylogeny(object):
         
         return assembly
 
-
-
-	def create_kmer(self,fileName,k):
+    def create_kmer(self,fileName,k):
         
-    		## @title : Create kmer from a gzip fasta file
+        ## @title : Create kmer from a gzip fasta file
         
-    		import re
-    		import gzip
-    		from Bio import SeqIO
+        import re
+        import gzip
+    	from Bio import SeqIO
         
-    		reverse_records = []
-    		records = []
+    	reverse_records = []
+    	records = []
         
-    		with gzip.open(fileName, "rt") as handle:
-        		for record in SeqIO.parse(handle, "fasta"):
-            		reverse = re.sub('[^GATC]', "", str(record.seq.reverse_complement()).upper())
-            		reverse_records.append(reverse)
-            		sequence = re.sub('[^GATC]', "", str(record.seq).upper())
-            		records.append(sequence)
-    		all_records = records + reverse_records
-    		dna_kmer=[]
-    		for dna in all_records:
-        		if len(dna)>=100:
-            			for i in range(len(dna)-k+1):
-                			dna_kmer.append(dna[i:i+k])
-        		else:
-            			print 'Tiny Genome'
-    		return dna_kmer
+    	with gzip.open(fileName, "rt") as handle:
+            for record in SeqIO.parse(handle, "fasta"):
+                reverse = re.sub('[^GATC]', "", str(record.seq.reverse_complement()).upper())
+            	reverse_records.append(reverse)
+            	sequence = re.sub('[^GATC]', "", str(record.seq).upper())
+            	records.append(sequence)
+    	all_records = records + reverse_records
+    	dna_kmer=[]
+    	for dna in all_records:
+            if len(dna)>=100:
+                for i in range(len(dna)-k+1):
+                    dna_kmer.append(dna[i:i+k])
+            else:
+                print 'Tiny Genome'
+    	return dna_kmer
 
 
 
